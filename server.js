ed00cebf7294fe8db7261450cec2c334def2c0a4
adrielze <oadriel14@gmail.com>
2024-03-27 16:57:45 -0300

    Adicionando configurações de endpoints

--- server.js
+++ server.js
@@ -27,13 +27,40 @@ app.get("/clients", function(req, res) { //https://jsonplaceholder.typicode.com/
 app.get("/clients/:id", function(req, res){
     const { id } = req.params
     const cliente = data.find(cli => cli.id == id) //procure nos dados o cliente cujo id é igual ao id da requisição
+
+    if(!cliente) return res.status(204).json() //Se não  há clientes, retornar a resposta 
+
+    res.json(cliente)
 })
 
-app.post("/clients", function(req, res){})
+app.post("/clients", function(req, res){
+    const { name, email } = req.body; //O body recebido(da requisição) será nesse formato
 
-app.put("/clients/:id", function(req, res){})
+    //salvar
 
-app.delete("/clients/:id", function(req, res){})
+    res.json({name, email})
+})
+
+app.put("/clients/:id", function(req, res){
+    const { id } = req.params
+    const cliente = data.find(cli => cli.id == id)
+
+    if(!cliente) return res.status(204).json()
+
+    const { name } = req.body;
+    cliente.name = name
+
+    res.json(cliente)
+})
+
+app.delete("/clients/:id", function(req, res){
+    const { id } = req.params
+    const clientesFiltrados = data.filter(cli => cli.id != id)
+
+    if(!clientesFiltrados) return res.status(204).json()
+
+    res.json(clientesFiltrados)
+})
  
 app.listen(3000, function() { //Inicia servidor na porta 3000
     console.log("Server is running")
